99ae08ec61d2ca3ea6b1f1fa5b3ef0464841e7a6
Robert Piechowski <robert.t.piechowski@gmail.com>
2022-04-03 15:21:44 +0200

    pierwszy commit

---
+++ src/main/java/pl/sda/arppl12/MainDebugger.java
@@ -0,0 +1,7 @@
+package pl.sda.arppl12;
+
+public class MainDebugger {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pl/sda/arppl12/Scanner.java
@@ -0,0 +1,7 @@
+package pl.sda.arppl12;
+
+public class Scanner {
+    public static void main(String[] args) {
+
+    }
+}
