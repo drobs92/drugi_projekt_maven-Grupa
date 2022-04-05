c6009409fa88c75342a1498aedc7130798841b8d
Robert Piechowski <robert.t.piechowski@gmail.com>
2022-04-05 18:43:08 +0200

    to jest komentarz -m

--- src/main/java/pl/sda/arppl12/Main.java
+++ src/main/java/pl/sda/arppl12/Main.java
@@ -3,5 +3,9 @@ package pl.sda.arppl12;
 public class Main {
     public static void main(String[] args) {
         System.out.println("gotowe");
+
+        // bla bla bla miał być jeden a są 3
+        //i znowu problem...
+        //zmiany do merga
     }
 }
